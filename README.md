4c1a35a0b97db3fe400e1d301c39c6b8fd414a5c
benjithorpe <benjaminthorpe19@gmail.com>
2021-04-15 19:00:48 +0000

    New Score feature included

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# random-guessing
-A random guessing game
+# Random Guessing
+
+A random guessing game that also keeps track of scores that has been saved.
+
+### Features
+- [X] Simple & easy to use.
+- [ ] Saves high score.
+- [ ] Display high scores even after closing browser.
--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 // Variables
 var saveScoreBtn = document.getElementById("saveBtn");
 var guessBtn = document.getElementById("guessBtn");
-// var level = document.querySelector(".level");
-var currentLevel = 1;
-var levelRange = 5;
+var initialScore = document.querySelector(".score");
+var initialLevel = document.querySelector(".level");
+var initialRange = document.querySelector(".range");
 var outputMessage = document.querySelector(".output");
-var secretNumber = Math.round(Math.random() * levelRange);
-// guessRangeInfo.innerText += ` ${levelRange}`;
+var guessRange = 5;
+var secretNumber = Math.round(Math.random() * guessRange);
 
 
+// Event Listeners
 saveScoreBtn.addEventListener('click', function(){
 	console.log("saving score...");
 	var currentLevel = document.querySelector(".level").innerText;
@@ -21,29 +22,42 @@ saveScoreBtn.addEventListener('click', function(){
 guessBtn.addEventListener('click', function(){
 	var userGuess = document.getElementById("guessNumber");
 
+	// return back if user does not enter any value
+	if(userGuess.value.trim() === ""){
+		return;
+	}
+
 	if(userGuess.value > secretNumber){
 		outputMessage.innerText = `${userGuess.value} is too high!! Try again`;
 	}else if(userGuess.value < secretNumber){
 		outputMessage.innerText = `${userGuess.value} is too low!! Try again`;
 	}else{
+		outputMessage.innerText = "Congratulations!! You won: Next Level";
 		updateGame();
 		userGuess.value = 0;
 	}
-	/*
-	console.log("Secret Number: " + secretNumber);
-	console.log("Number: " + userGuess.value);
-	console.log("LevelRange: " + levelRange);
-	*/
 });
 
 function changeSecretNumber(){
-	secretNumber = Math.round(Math.random() * levelRange) + 1;	
+	secretNumber = Math.round(Math.random() * guessRange);
+	console.log(secretNumber);
 }
 
 function updateGame(){
-	level.innerText = `Level: ${++currentLevel}`;
-	outputMessage.innerText = "Congratulations!! You won: Next Level";
+	// increase score points at level 10
+	if(Number(initialLevel.innerText) >= 10){
+		increaseScore(10);
+	}else if(Number(initialLevel.innerText) >= 25){
+		increaseScore(20);
+	}else{
+		increaseScore(5);
+	}
+	guessRange += 5;
 	changeSecretNumber();
-	// guessRangeInfo.innerText = `Guess a number between 1 and ${levelRange += 5}`;
 }
 
+function increaseScore(score){
+	initialScore.innerText = Number(initialScore.innerText) + score;	
+	initialRange.innerText = Number(initialRange.innerText) + 5;
+	initialLevel.innerText = Number(initialLevel.innerText) + 1;
+}
