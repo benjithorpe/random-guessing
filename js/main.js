c873a5f62a89e4c30dd0332a9b8c78f68b9c76e6
benjithorpe <benjaminthorpe19@gmail.com>
2021-04-15 13:19:34 +0000

    Redesign the whole page, add a scores area

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+// Variables
+var saveScoreBtn = document.getElementById("saveBtn");
 var guessBtn = document.getElementById("guessBtn");
-var level = document.querySelector(".level");
-var levelCount = 1;
+// var level = document.querySelector(".level");
+var currentLevel = 1;
 var levelRange = 5;
 var outputMessage = document.querySelector(".output");
 var secretNumber = Math.round(Math.random() * levelRange);
-var guessRangeInfo = document.querySelector(".msg");
-guessRangeInfo.innerText += ` ${levelRange}`;
+// guessRangeInfo.innerText += ` ${levelRange}`;
 
+
+saveScoreBtn.addEventListener('click', function(){
+	console.log("saving score...");
+	var currentLevel = document.querySelector(".level").innerText;
+	var currentScore = document.querySelector(".score").innerText;
+
+	console.log("Level = " + currentLevel);
+	console.log("Score = " + currentScore);
+});
 
 guessBtn.addEventListener('click', function(){
 	var userGuess = document.getElementById("guessNumber");
@@ -31,8 +41,9 @@ function changeSecretNumber(){
 }
 
 function updateGame(){
-	level.innerText = `Level: ${++levelCount}`;
+	level.innerText = `Level: ${++currentLevel}`;
 	outputMessage.innerText = "Congratulations!! You won: Next Level";
 	changeSecretNumber();
-	guessRangeInfo.innerText = `Guess a number between 1 and ${levelRange += 5}`;
+	// guessRangeInfo.innerText = `Guess a number between 1 and ${levelRange += 5}`;
 }
+
